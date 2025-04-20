071cd5995e1d20a7f2e3872738da1d99da18a7c2
rafawang2 <rafawcy@gmail.com>
2025-04-21 00:57:00 +0800

    新增AlphaZero MCTS實作 -目前還沒有valueNet，目前架構非完整MCTS -以ResNetPlayer作為policyNet -評估函數還是以傳統MCTS為主

---
+++ arg.py
@@ -0,0 +1,22 @@
+m = 5
+n = 5
+
+# batch_size = (m * (n - 1)) * 2
+# batch_size = (2*m-1)*(2*n-1)
+# batch_size = 1
+batch_size = 16
+'''
+type 0: normal;             input shape: m * n
+type 1: history_image;      input shape: m * n * total_move
+type 2: history_sequence;   input shape: total_move * (m * n)
+type 3: history_video;      input shape: total_move * m * n * 1
+'''
+args_Res = {
+    'num_of_generate_data_for_train': 250,
+    'epochs': 50,
+    'batch_size': batch_size,
+    'verbose': True,
+    'type': 0,
+    'train': True,
+    'load_model_name': None
+}
