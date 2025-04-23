fe1bc0824cb36007bd7641fb1f842160c90cf333
rafawang2 <rafawcy@gmail.com>
2025-04-23 23:17:15 +0800

    遊戲規則檢查修改 先檢查move是不是valid，才進行移動

--- Dots_and_Box.py
+++ Dots_and_Box.py
@@ -6,43 +6,6 @@
 from DaB_Utils import *
 from dataclasses import dataclass, field
 from typing import List
-def ANSI_string(s="", color=None, background=None, bold=False):
-    colors = {
-        'black': '\033[30m',
-        'red': '\033[31m',
-        'green': '\033[32m',
-        'yellow': '\033[33m',
-        'blue': '\033[34m',
-        'magenta': '\033[35m',
-        'cyan': '\033[36m',
-        'white': '\033[37m',
-        'reset': '\033[0m'
-    }
-
-    background_colors = {
-        'black': '\033[40m',
-        'red': '\033[41m',
-        'green': '\033[42m',
-        'yellow': '\033[43m',
-        'blue': '\033[44m',
-        'magenta': '\033[45m',
-        'cyan': '\033[46m',
-        'white': '\033[47m',
-        'reset': '\033[0m',
-        'gray': '\033[100m',  # 新增的灰色背景
-        'light_gray': '\033[47m'  # 新增的淺灰色背景
-    }
-
-    styles = {
-        'bold': '\033[1m',
-        'reset': '\033[0m'
-    }
-    color_code = colors[color] if color in colors else ''
-    background_code = background_colors[background] if background in colors else ''
-    bold_code = styles['bold'] if bold else ''
-
-    return f"{color_code}{background_code}{bold_code}{s}{styles['reset']}"
-
 
 @dataclass
 class STATE:
@@ -98,12 +61,14 @@ class DotsAndBox():
             else:
                 move_data = player2.get_move(self.state.board, self.state.current_player)
 
-            if move_data[0]:    #valid_postion
+            if move_data[0]:
                 row, col = move_data[0]
+                # 進行落子並檢查換手
+                if not isValid(self.state.board, row, col):
+                    print("invalid move!")
+                    continue
                 if train:
                     self.history.append(move_data[1])
-
-                # 進行落子並檢查換手
                 self.state.current_player,score = make_move(self.state.board, row, col, self.state.current_player)
                 if self.state.current_player == -1:
                     self.state.p1_p2_scores[0] += score
